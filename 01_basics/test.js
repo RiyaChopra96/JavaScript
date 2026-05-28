4e7d7b9b7dcb30998c5ed99155dbd6b886a2e69e
RiyaChopra96 <criya2409@gmail.com>
2026-05-28 10:01:11 +0000

    Container config Done

---
+++ 01_basics/test.js
@@ -0,0 +1,14 @@
+const accountId = 58798
+let accountEmail="abcgoogle.com"
+var acountPassword="12345"
+let accountCity="prayagraj"
+let accountState
+
+accountEmail="ac.google.com"
+accountPassword="212121"
+accountCity="mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountState]
+
+);
