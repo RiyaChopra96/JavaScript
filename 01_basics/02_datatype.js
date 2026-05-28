4c047d1e9ffbb26df00eec4c1d60522856964b31
RiyaChopra96 <criya2409@gmail.com>
2026-05-28 10:31:03 +0000

    Container condig Done

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,3 +1,14 @@
 "use strict"; //treat all code as newer versions
 
 console.log("Hello");
+
+//we are working on the node.js
+//ECMA standard deffine the rules,syntax and guidelines of js code
+
+let state;
+console.log(state);
+console.log(typeof state);  //undef-no value
+
+let temp=null;
+console.log(temp);
+console.log(typeof temp);  //null-object
